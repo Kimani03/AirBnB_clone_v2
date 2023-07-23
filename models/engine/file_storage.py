7aed884ec93694ba8b28a364bd538c080f9cdf30
Kimani03 <victorkariuki03@gmail.com>
2023-07-23 15:27:00 +0300

    Added files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,14 +9,20 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """Returns a list of objects of one type of class.
+
+        Args:
+            cls (any, optional): Class of object. Defaults to None.
+
+        Returns:
+            list: list of objects of one type of class.
+        """
         if cls is None:
             return FileStorage.__objects
-        cls_objs = {}
-        for key, value in self.__objects.items():
-            if value.__class__ == cls:
-                cls_objs[key] = value
-        return cls_objs
+        return {
+            key: value for key, value in self.__objects.items()
+            if type(value) is cls
+        }
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -33,15 +39,15 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
         from models.amenity import Amenity
+        from models.base_model import BaseModel
+        from models.city import City
+        from models.place import Place
         from models.review import Review
+        from models.state import State
+        from models.user import User
 
-        clses = {
+        classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
@@ -51,17 +57,24 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    FileStorage.__objects[key] = clses[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Delete obj from __objects if it’s inside"""
-        if obj:
-            del_obj = obj.__class__.__name__ + '.' + obj.id
-            del(FileStorage.__objects[del_obj])
+        """Delete obj from __objects
+
+        If obj is equal to None, the method should not do anything.
+        Args:
+            obj (models.class.<any model>, optional): object to delete.
+            Defaults to None.
+        """
+        if obj in self.__objects.values():
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del(self.__objects[key])
+        return
 
     def close(self):
-        """Calls reload() method for deserializing the JSON file to objects"""
+        """Calls reload method for deserializing the JSON file to objects
+        """
         self.reload()
-        
